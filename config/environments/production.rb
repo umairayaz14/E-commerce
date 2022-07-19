0a43780575917f96e78d2827d8278a161bb10200
Umair Ayaz Aslam <umair.ayaz@devsinc.com>
2022-07-19 16:22:41 +0500

    Adding smtp to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,17 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'https://aqueous-citadel-74242.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: Rails.application.credentials[:user_name],
+    password: Rails.application.credentials[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
